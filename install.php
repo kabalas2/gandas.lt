60f34350b8dd4c0f225ddafd74dbc31949c2724e
Arnoldas Turulis <arnoldas.t@emotion.lt>
2022-03-28 21:07:32 +0300

    added install script

--- install.php
+++ install.php
@@ -1,4 +1,17 @@
 <?php 
+include 'config.php';
+
+
+try {
+    $conn = new PDO("mysql:host=".DB_HOST.";dbname=".DB_NAME, DB_USER, DB_PASS);
+    // set the PDO error mode to exception
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo "Connected successfully";
+} catch(PDOException $e) {
+    echo "Connection failed: " . $e->getMessage();
+}
+
+
 
 $sql = "SET NAMES utf8;
 SET time_zone = '+00:00';
@@ -75,3 +88,5 @@ CREATE TABLE `users` (
   `created_at` datetime NOT NULL ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;";
+
+$conn->query($sql);
