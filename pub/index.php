80478b7da83b25a2236b562c511000fb05a7a47d
Arnoldas Turulis <arnoldas.t@emotion.lt>
2022-03-29 20:59:20 +0300

    debug mode + error showing

--- pub/index.php
+++ pub/index.php
@@ -3,6 +3,12 @@
 include '../config.php';
 include '../vendor/autoload.php';
 
+if (DEBUG_MODE) {
+    ini_set('display_errors', 1);
+    ini_set('display_startup_errors', 1);
+    error_reporting(E_ALL);
+}
+
 $router = new Core\Router();
 $launcher = new Core\Launcher();
 
