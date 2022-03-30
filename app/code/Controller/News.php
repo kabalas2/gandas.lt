e3e8bf513047798f977f622de52af12e967ca30d
Arnoldas Turulis <arnoldas.t@emotion.lt>
2022-03-30 21:18:36 +0300

    twig rendering

--- app/code/Controller/News.php
+++ app/code/Controller/News.php
@@ -9,8 +9,6 @@ class News extends ControllerAbstract
     public function show(string $slug){
         $new = new \Model\News();
         $new->loadBySlug($slug);
-        $template$this->twig
-        // renderinsiu templeita ir siusiu naujienos objekta
-        // atvaizdavimui.
+        echo $this->twig->render('single.html', ['new'=> $new]);
     }
 }
