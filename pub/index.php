b6b6add72fb8e551d9d3fb39d75c84f32973e95c
Arnoldas Turulis <arnoldas.t@emotion.lt>
2022-03-31 20:08:57 +0300

    bootstrap styles

--- pub/index.php
+++ pub/index.php
@@ -4,9 +4,9 @@ include '../config.php';
 include '../vendor/autoload.php';
 
 if (DEBUG_MODE) {
-    ini_set('display_errors', 1);
-    ini_set('display_startup_errors', 1);
-    error_reporting(E_ALL);
+    ini_set('display_errors', 0);
+    ini_set('display_startup_errors', 0);
+    //error_reporting(E_ALL);
 }
 
 $router = new Core\Router();
